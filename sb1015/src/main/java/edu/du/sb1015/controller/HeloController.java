1bb45b3b6cfc0c72ee9a4dfcdd9145d139196c2c
yunjh1219 <orenjh1219@gmail.com>
2024-10-15 18:30:54 +0900

    24-10-15

---
+++ sb1015/src/main/java/edu/du/sb1015/controller/HeloController.java
@@ -0,0 +1,85 @@
+package edu.du.sb1015.controller;
+
+import edu.du.sb1015.dto.DataObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+
+@Controller
+public class HeloController {
+
+//    @GetMapping("/")
+    public String helo(Model model) {
+        model.addAttribute("msg", "이름을 적어 주세요");
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String heloPost(@RequestParam("text1") String name, Model model) {
+        model.addAttribute("value", name);
+        model.addAttribute("msg", "당신의 이름은 "+name+"이군요!");
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg","message 1<hr/>message 2<br/>message 3");
+        DataObject obj = new DataObject(123, "lee", "lee@flower");
+        model.addAttribute("object", obj);
+       return "index";
+    }
+
+    @GetMapping("/id/{id}")
+    public String index(@PathVariable int id, Model mav){
+        mav.addAttribute("id", id);
+        mav.addAttribute("check", id>=0);
+        mav.addAttribute("trueVal", "POSITIVE!");
+        mav.addAttribute("falseVal", "NEGATIVE..");
+        DataObject obj = new DataObject(123, "lee", "lee@flower");
+        mav.addAttribute("object",obj);
+        return "index";
+    }
+
+    @RequestMapping("/month/{month}")
+    public  ModelAndView index(@PathVariable int month, ModelAndView mav) {
+        mav.setViewName("index2");
+        int m = Math.abs(month) % 12;
+        m = m == 0 ? 12 : m;
+        mav.addObject("month", m);
+        mav.addObject("check", Math.floor(m/3));
+        return mav;
+    }
+
+    @RequestMapping("/index3")
+    public ModelAndView index3(ModelAndView mav) {
+        mav.setViewName("index3");
+        ArrayList<String[]> data = new ArrayList<String[]>();
+        data.add(new String[]{"park","park@yamada","090-999-999"});
+        data.add(new String[]{"lee","lee@yamada","080-888-8888"});
+        data.add(new String[]{"yun","yun@yamada","070-777-7777"});
+        mav.addObject("data", data);
+        return mav;
+    }
+
+    @RequestMapping("/index4")
+    public ModelAndView index4(ModelAndView mav) {
+        mav.setViewName("index4");
+        ArrayList<String[]> data = new ArrayList<String[]>();
+        data.add(new String[]{"park","park@yamada","090-999-999"});
+        data.add(new String[]{"lee","lee@yamada","080-888-8888"});
+        data.add(new String[]{"yun","yun@yamada","070-777-7777"});
+        mav.addObject("data", data);
+        return mav;
+    }
+
+    @RequestMapping("/tax/{tax}")
+    public ModelAndView index4(@PathVariable int tax, ModelAndView mav) {
+        mav.setViewName("index5");
+        mav.addObject("tax", tax);
+        return mav;
+    }
+
+}
---
+++ sb1015_2/src/main/java/edu/du/sb1015_2/Sb10152Application.java
@@ -0,0 +1,51 @@
+package edu.du.sb1015_2;
+
+import edu.du.sb1015_2.entity.MyData;
+import edu.du.sb1015_2.repository.MyDataRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class Sb10152Application {
+
+    //    @Autowired
+    final MyDataRepository repository;
+
+//    public Sb10152Application(MyDataRepository repository) {
+//        this.repository = repository;
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb10152Application.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        MyData d1 = new MyData();
+        d1.setName("kim");
+        d1.setAge(123);
+        d1.setMail("kim@gilbut.co.kr");
+        d1.setMemo("this is my data!");
+        repository.saveAndFlush(d1);
+
+        MyData d2 = new MyData();
+        d2.setName("lee");
+        d2.setAge(15);
+        d2.setMail("lee@flower");
+        d2.setMemo("my girl friend.");
+        repository.saveAndFlush(d2);
+
+        MyData d3 = new MyData();
+        d3.setName("choi");
+        d3.setAge(37);
+        d3.setMail("choi@happy");
+        d3.setMemo("my work friend...");
+        repository.saveAndFlush(d3);
+    }
+
+}
