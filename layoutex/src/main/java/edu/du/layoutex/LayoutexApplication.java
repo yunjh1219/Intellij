0d36dc5c7180fae3718fdc2c4793b317f1d1b406
yunjh1219 <orenjh1219@gmail.com>
2024-10-29 19:43:29 +0900

    layoutex = 기본틀, 클릭 시 링크 색상 변경 고정되게

---
+++ layoutex/src/main/java/edu/du/layoutex/LayoutexApplication.java
@@ -0,0 +1,13 @@
+package edu.du.layoutex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayoutexApplication.class, args);
+    }
+
+}
---
+++ layoutex/src/main/java/edu/du/layoutex/controller/LayoutController.java
@@ -0,0 +1,15 @@
+package edu.du.layoutex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LayoutController {
+
+    @GetMapping
+    public String main(){
+        return "page/main";
+    }
+}
